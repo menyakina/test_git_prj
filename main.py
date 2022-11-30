37548b69ba80639af27a572e25fb642163134c31
menyakina <menyakinaz@mail.ru>
2022-11-30 16:05:07 +0300

    создали класс Rectangle Добавили метод Добавили set и p area for circle

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from math import pi
+
+
 class Rectangle:
     def __init__(self, a, b):
         self.a = a
@@ -6,12 +9,17 @@ class Rectangle:
     def area(self):
         return self.a * self.b
 
+
 class Circle:
     def __init__(self, r):
         self.r = r
 
+    def area(self):
+        return pi * self.r**2
+
 
 main_rect = Rectangle(5, 4)
 print(main_rect.area())
-main_circle = Circle(2)
+main_circle = Circle(7)
+print(main_circle.area())
 
