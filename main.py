2d8eaefdecbcf793e0abca20030326cc3a7a6d8f
menyakina <menyakinaz@mail.ru>
2022-11-30 14:19:15 +0300

    создали класс Rectangle Добавили метод Добавили set и perimetr

--- main.py
+++ main.py
@@ -6,6 +6,21 @@ class Rectangle:
     def area(self):
         return self.a * self.b
 
+    def get_height(self):
+        return self.a
+
+    def get_width(self):
+        return self.b
+
+    def perimetr(self):
+        return self.a * 2 + self.b * 2
+    def set_heigt(self, a):
+        self.a = a
+
+        def set_width(self, b):
+            self.b = b
+
 
 main_rect = Rectangle(5, 4)
 print(main_rect.area())
+print(main_rect.perimetr())
